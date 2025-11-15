04b2d739c491735e719e837c6a48333cdc47178d
jmabilon <jmabilon@sqli.com>
2025-11-15 14:38:08 +0100

    feat: add navigation dependency and remove hidden package

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.navigation.compose)
 
             // Koin
             implementation(libs.bundles.common.koin)
